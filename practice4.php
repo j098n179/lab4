f964ab451cb2b479a52892aa62d8c93bb10808c2
Jacob <143302960+j098n179@users.noreply.github.com>
2025-02-27 19:59:42 -0600

    Add files via upload

--- practice4.php
+++ practice4.php
@@ -1,11 +1,11 @@
 <?php 
-if ($_POST) { 
-    $num = $_POST["usernum"]; 
-    for ($x = 1; $x <= $num; $x++)
+if ($_POST) { //checks for when post has been sent over. referenced code from here https://www.geeksforgeeks.org/program-to-print-multiplication-table-of-any-number-in-php/ to help make the table
+    $num = $_POST["usernum"]; //grab the number that was submitted by the user
+    for ($x = 1; $x <= $num; $x++) //loops through the column from 1 to the user number
     { 
-        for ($y = 1; $y <= $num; $y++)
+        for ($y = 1; $y <= $num; $y++) //loops through the rows from 1 to the user number
         { 
-            echo "$x*$y = " . $x * $y . "<br>"; 
+            echo "$x*$y = " . $x * $y . "<br>";  //prints out the multiplication table
         }
     } 
 } 
